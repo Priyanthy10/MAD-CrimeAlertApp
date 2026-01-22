d52ef4f43c3e0c0fc7842d6aeb51fdf2a6bd188e
Jiolashington Ben Asher <155675926+Jbenash@users.noreply.github.com>
2026-01-22 17:57:25 +0530

    chore: updated firebase configuration and project structure

--- crimeAlert-frontend/src/services/firebaseConfig.ts
+++ crimeAlert-frontend/src/services/firebaseConfig.ts
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA-n20EKvJNcc9WDe3KXb5XZsoY8oz7FIw",
-    authDomain: "crimealert-55797.firebaseapp.com",
-    projectId: "crimealert-55797",
-    storageBucket: "crimealert-55797.firebasestorage.app",
-    messagingSenderId: "606553032997",
-    appId: "1:606553032997:web:87a76ed9b5a806fd5bb2cc"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
